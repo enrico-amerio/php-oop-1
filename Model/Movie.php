ddc213943f3ed0a944c030b47ae8959460a75f03
enrico-amerio <amerio.enrico95@gmail.com>
2024-05-02 17:00:26 +0200

    [add]  Print in page movies

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,18 +1,21 @@
 <?php 
  class Movie{
   private $title;
-  public $genre;
+  public $genres;
   public $year;
   
-  public function __construct(string $_title, array $_genre, int $_year)
+  public function __construct(string $_title, array $_genres, int $_year)
   {
     $this->setTitle($_title);
-    $this->genre = $_genre;
+    $this->genres = $_genres;
     $this->year = $_year;
   }
 
   public function setTitle($_title){
    $this->title = $_title;
+  }
+  public function getTitle(){
+    return $this->title;
   }
  }
 
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-  require_once __DIR__ . '/Model/Movie.php'
-  
-?>
+  require_once __DIR__ . '/Model/Movie.php';
+  $alien = new Movie('Alien', ['Horror', 'Thriller', 'Action'], 1979);
+  $jaws = new Movie('Jaws', ['Horror','Action','Drama'], 1975);
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +11,39 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <h2>
+  <?php
+  echo  $alien->getTitle();
+  ?>
+  </h2>
+  <ul>
+    <?php foreach ($alien->genres as $genre) : ?>
+      <li>
+          <?php echo $genre ?>
+      </li>
+    <?php endforeach ?>
+  </ul>
+  <p>
+  <?php
+  echo  $alien->year;
+  ?>
+  </p>
+  <h2>
+  <?php
+  echo  $jaws->getTitle();
+  ?>
+  </h2>
+  <ul>
+    <?php foreach ($jaws->genres as $genre) : ?>
+      <li>
+          <?php echo $genre ?>
+      </li>
+    <?php endforeach ?>
+  </ul>
+  <p>
+  <?php
+  echo  $jaws->year;
+  ?>
+  </p>
 </body>
 </html>
